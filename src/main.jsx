d49c5c1c5c380150e9f903365af32e37cc84ccff
eugeniusz57 <104994034+eugeniusz57@users.noreply.github.com>
2023-04-30 15:17:27 +0200

    404

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="https://golden-nougat-dd8985.netlify.app/">
+    <BrowserRouter basename="/golden-nougat-dd8985.netlify.app/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
